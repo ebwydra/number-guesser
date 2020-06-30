f0fecfa70c955b449cf3f52f2ddb1a107793593f
ebwydra <ebwydra@gmail.com>
2020-06-30 17:32:55 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+// Generates a random integer between 0 and 9
+function generateTarget() {
+  return Math.floor(Math.random() * 10)
+}
+
+// Determines which guess is closest to the target number
+function compareGuesses(humanGuess, computerGuess, targetNum) {
+  // Absolute value between human guess and target
+  const humanDist = Math.abs(humanGuess - targetNum);
+  // Absolute value between computer guess and target
+  const computerDist = Math.abs(computerGuess - targetNum);
+  if (humanDist <= computerDist) { // if human guess it closer (or guesses equal)
+    return true;
+  } else { // if computer guess is closer
+    return false;
+  }
+}
+
+/*
+// Testing out the compareGuesses function...
+console.log(compareGuesses(1, 9, 2)) // should be true
+console.log(compareGuesses(1, 1, 2)) // should be true
+console.log(compareGuesses(9, 1, 2)) // should be false
+*/
+
+// Increases the winner's score after each round
+function updateScore(winner) {
+  if (winner === 'human') {
+    humanScore ++;
+  } else if (winner === 'computer') {
+    computerScore ++;
+  } else {
+    console.log('Invalid input!')
+  }
+}
+
+// Updates the round number after each round
+function advanceRound() {
+  currentRoundNumber ++;
+}
